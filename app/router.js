4aec64f10ee456b97de585d30f5248ca14593725
Manav Misra <manavm@visionwebsoft.com>
2021-08-31 20:39:25 -0500

    Log test message from api route

--- app/router.js
+++ app/router.js
@@ -2,6 +2,8 @@ import Router from "express";
 
 const router = new Router();
 
-// TODO: Add routes here (maybe 🤔 start with a GET test route)
+router.get("/", (_, res) => {
+  res.send("Hello World from API");
+});
 
 export default router;
