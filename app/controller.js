1181266bae57456065b82e4df23bb6d6e85925d5
Manav Misra <manavm@visionwebsoft.com>
2021-08-31 21:21:18 -0500

    Add functionality to create and get by id

---
+++ app/controller.js
@@ -0,0 +1,18 @@
+// Uses the db client from loader.js
+import { ObjectId } from "mongodb";
+import client from "./loader.js";
+
+const notesConnection = client.db("noteTaker").collection("notes");
+
+export default {
+  index() {
+    return notesConnection.find({}).toArray();
+  },
+  create(newNote) {
+    // Returns a promise
+    return notesConnection.insertOne(newNote);
+  },
+  show(id) {
+    return notesConnection.findOne(ObjectId(id));
+  },
+};
--- app/router.js
+++ app/router.js
@@ -1,4 +1,5 @@
 import Router from "express";
+import noteController from "./controller.js";
 
 const router = new Router();
 
@@ -6,8 +7,31 @@ router.get("/", (_, res) => {
   res.send("Hello World from API");
 });
 
-router.post("/", (req, res) => {
-  res.json(req.body);
+router.get("/notes", async (_, res) => {
+  try {
+    const notes = await noteController.index();
+    res.json(notes);
+  } catch (error) {
+    res.status(400).send(error);
+  }
+});
+
+router.get("/notes/:id", async (req, res) => {
+  try {
+    const note = await noteController.show(req.params.id);
+    res.json(note);
+  } catch (error) {
+    res.status(400).send(error);
+  }
+});
+
+router.post("/notes", async (req, res) => {
+  try {
+    const newNote = await noteController.create(req.body);
+    res.status(201).json(newNote);
+  } catch (error) {
+    res.status(400).json(error.message);
+  }
 });
 
 export default router;
